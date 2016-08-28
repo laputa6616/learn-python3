1c5ff706f802bed93e8a48de0ee008033c1c1e51
Lawrenauh <laputa6616@gmail.com>
2016-08-28 23:25:38 +0800

    update REAME.md and learning_notes.py

--- learning_notes.py
+++ learning_notes.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
 #coding:utf-8
 
-1.切片：一种可变的有序集合
+1 列表(list)：一种可变的有序集合
 
-		list[n:m:s]
+	1.1 列表的创建和列表元素的访问
+		
+		e.g.
+			classmates = ['hhqy', 'Nauh', 'laputa']
+			
+			classmates[index_num]
+			
+		注意：
+		(1) Python 的索引号都是从 0 开始的；
+		(2) index_num 的取值范围为 [-len(classmates), len(classmates)-1]；
+		(3) 列表中元素可以是不同的数据类型；
+		(4) 空列表：void = []；
+		(5) 列表中的元素也可以是列表，可以用索引递归访问，如：list[3][2] = (list[3])[2]
 
-  表示取列表list中索引号从 n 开始，步长为 s，到（m-1）结束的元素所构成的list。
-  
-  注意：
-  (1) Python 的索引号都是从 0 开始的；
-  (2) 负数切片 list[-n:] 表示依次取列表list后 n 位元素所构成的list，此时步长仍为1；
+	1.2 列表操作：
+		list.append(new_value)					// 追加新元素至末尾
+		list.insert(index_num, new_value)		// 把新元素插入指定索引位置 
+		list.pop(index_num)/list.pop()			// 删除置顶索引位置元素或直接删除最后一个元素
+		
+		list[index_num] = new_value				// 用新值替换指定索引值
 
-  (3) 其他比较特殊、需要注意的切片：
+	1.3 列表切片：
+	
+			list[n:m:s]
+	
+		表示取列表list中索引号从 n 开始，步长为 s，到（m-1）结束的元素所构成的list。
+	
+		注意：
+		(1) 负数切片 list[-n:] 表示依次取列表list后 n 位元素所构成的list，此时步长仍为1；
+		(2) 其他比较特殊、需要注意的切片：
 
-  	  list[:]     原样复制list；
-  	  list[::2]   从原list中「第一个元素」开始，以步长为2截取切片，即「偶数」索引切片，类似的「奇数」索引切片：list[1::2]；
-  	  list[::-1]  反向复制list (原list「倒序」排列)；
-
-  (4) tuple 和 字符串 也可以用切片操作，结果仍为 tuple 和 字符串:
-  e.g. (0, 1, 2, 3)[:2] = (0, 1)
-       'ABCD'[:2] = 'AB'
+			  list[:]     原样复制list；
+			  list[::2]   从原list中「第一个元素」开始，以步长为2截取切片，即「偶数」索引切片，类似的「奇数」索引切片：list[1::2]；
+			  list[::-1]  反向复制list (原list「倒序」排列);
+		
+		(3) tuple 和 字符串 也可以用切片操作，结果仍为 tuple 和 字符串，操作方法与列表类似:
+		e.g. (0, 1, 2, 3)[:2] = (0, 1)
+		     'ABCD'[:2] = 'AB'
